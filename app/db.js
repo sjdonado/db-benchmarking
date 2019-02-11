19e1cc5c1d2af9535fd332ead132574d7d1c52ef
Juan Rodriguez <jsrd98@gmail.com>
2019-02-10 23:50:05 -0500

    fix: db connection, npm install prefix

--- app/db.js
+++ app/db.js
@@ -1,29 +1,12 @@
 const mysql = require('mysql');
-const { MongoClient, Server } = require('mongodb');
-
+const { connect } = require('mongodb');
 
 // Set up the connection to the local db
-const mongoclient = new MongoClient(new Server("localhost", 27017), {native_parser: true});
 
-// Open the connection to the server
-mongoclient.open(function(err, mongoclient) {
-  // Get the first db and do an update document on it
-  var db = mongoclient.db("integration_tests");
-  db.collection('mongoclient_test').update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-    assert.equal(null, err);
-    assert.equal(1, result);
-
-    // Get another db and do an update document on it
-    var db2 = mongoclient.db("integration_tests2");
-    db2.collection('mongoclient_test').update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-      assert.equal(null, err);
-      assert.equal(1, result);
-
-      // Close the connection
-      mongoclient.close();
-    });
+const mongoclient = connect("mongodb://mongodb:27017/Benchmark", { useNewUrlParser: true })
+  .then(res => {
+    console.log("Mongodb Connected!");
   });
-});
 
 exports.mongoclient = mongoclient;
 
@@ -36,7 +19,7 @@ const mysqlClient = mysql.createConnection({
 
 mysqlClient.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Mysql Connected!");
 });
 
 exports.mysqlClient = mysqlClient;
