924bbf0eaded7ecfecafe1738e7d7195439c3ef7
Brian Ramirez <brians@uninorte.edu.co>
2019-02-25 15:05:32 -0500

    refactor db refactorized

--- app/db.js
+++ app/db.js
@@ -1,26 +1,12 @@
 const mysql = require('mysql');
 const { connect } = require('mongodb');
-
 // Set up the connection to the local db
-
 const mongoclient = connect("mongodb://mongodb:27017/Benchmark", { useNewUrlParser: true })
-  .then(res => {
-    console.log("Mongodb Connected!");
-  });
-
-exports.mongoclient = mongoclient;
-
-
 const mysqlClient = mysql.createConnection({
   host: "mysql",
   user: "tester",
   password: "testerpass"
 });
-
-mysqlClient.connect(function(err) {
-  if (err) throw err;
-  console.log("Mysql Connected!");
-});
-
+exports.mongoclient = mongoclient;
 exports.mysqlClient = mysqlClient;
 
